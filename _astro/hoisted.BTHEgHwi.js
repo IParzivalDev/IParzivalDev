67e030917ceb9cab11792caf3b0b966241cfa43f
IParzivalDev <iparzivalmc@proton.me>
2024-11-05 21:57:27 -0400

    Redesigning the website and switching to Astro

---
+++ _astro/hoisted.BTHEgHwi.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",function(){const e=document.getElementById("loading-screen");e&&(e.style.opacity="0",setTimeout(function(){e.style.display="none"},500))});
---
+++ _astro/hoisted.CCO2i5Kr.js
@@ -0,0 +1,1 @@
+import"./hoisted.BTHEgHwi.js";import"./hoisted.DYpF8O5E.js";
